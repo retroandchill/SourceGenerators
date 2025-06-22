4a02110d7f3da3bc6a304848830b25d7926cd012
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-06-22 13:52:49 -0400

    Updated a small amount of test coverage info

--- SourceGeneratorUtilities/src/Retro.SourceGeneratorUtilities/Utilities/XmlCommentUtils.cs
+++ SourceGeneratorUtilities/src/Retro.SourceGeneratorUtilities/Utilities/XmlCommentUtils.cs
@@ -6,13 +6,25 @@ using RhoMicro.CodeAnalysis;
 
 namespace Retro.SourceGeneratorUtilities.Utilities;
 
+/// <summary>
+/// Provides utility methods for processing and extracting data from XML-based documentation comments.
+/// </summary>
 #if SOURCE_UTILS_GENERATOR
 [IncludeFile]
 #endif
-public static class XmlCommentUtils {
+internal static class XmlCommentUtils {
 
-  private static readonly Regex Trimmer = new(@"\s\s+");
+  private static readonly Regex Trimmer = new(@"\s\s+", RegexOptions.None, TimeSpan.FromSeconds(1));
 
+  /// <summary>
+  /// Extracts and trims the content of the &lt;summary> tag from an XML comment string.
+  /// The method removes excessive whitespace, normalizing it to single spaces.
+  /// </summary>
+  /// <param name="xmlComment">The XML comment as a string. Can be null, empty, or contain whitespace.</param>
+  /// <returns>
+  /// A string containing the trimmed content of the &lt;summary> tag if it exists; null if the tag is absent,
+  /// the input is null, only whitespace, or improperly formatted.
+  /// </returns>
   public static string? GetSummaryTag(this string? xmlComment) {
     if (string.IsNullOrWhiteSpace(xmlComment)) {
       return null;
